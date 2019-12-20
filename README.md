226851438fcca2b4f655067006716ac2b43a6dd7
YuChen Liu <yuchen.liu@grabtaxi.com>
2019-12-20 14:54:48 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # apiservice-demo
 
+## Build image
+view `build-img.sh` for detail
+
+## Push your image to aws ECR
+view `push-img.sh` for detail
--- k8s-tools-and-definations/cluster-startup.sh
+++
@@ -1,4 +0,0 @@
-kubeadm init --pod-network-cidr=10.244.0.0/16
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
-
-./create-secret.sh
